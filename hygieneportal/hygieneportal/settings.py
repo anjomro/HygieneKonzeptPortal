4c78fb0590b15939819fba273f7cb0f7a6771f40
Anton Mrosek <github@anjomro.de>
2020-09-18 19:55:25 +0200

    Enabled production mode

--- hygieneportal/hygieneportal/settings.py
+++ hygieneportal/hygieneportal/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if 'DJANGO_DEBUG' in os.environ:
     DEBUG = True
 
