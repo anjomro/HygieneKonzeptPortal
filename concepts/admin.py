21bd637404d6a67aec41ce8cc843eff8ee5a1613
Anton Mrosek <github@anjomro.de>
2020-09-18 18:06:21 +0200

    Moved files

--- concepts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Club, Contact, Concept
-# Register your models here.
-admin.site.register(Club)
-admin.site.register(Concept)
-admin.site.register(Contact)
--- concepts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConceptsConfig(AppConfig):
-    name = 'concepts'
